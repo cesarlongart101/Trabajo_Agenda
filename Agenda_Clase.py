b6c90291af62526ab572c6cc2ab9836bec8dcbc9
cesarlongart101 <cesarlongart101@gmail.com>
2023-08-03 21:42:57 +0200

    Primera prueba exitosa

--- Agenda_Clase.py
+++ Agenda_Clase.py
@@ -1,27 +1,24 @@
 class Agenda:
 
 
-    def obten_telefono(fichero, persona):
+    def obten_telefono(Self, fichero, persona):
         with open(fichero, "r") as archivo:
             for linea in archivo:
-                print(linea)
-        pass
+                if persona in linea:
+                    print(linea)
+                    return
+            print("No existe en la agenda.")
 
-    def inserta_telefono(fichero, persona, teléfono):
-        nombre = "Juan"
-        telefono = "123456789" 
+    def inserta_telefono(Self, fichero, persona, telefono):
+        with open(fichero, "a") as archivo:
+            archivo.write(persona + "," + telefono + "\n")
 
-        with open("contactos.txt", "a") as archivo:
-            archivo.write(nombre + " , " + telefono + "\n")
-        pass
-
-    def elimina_telefono(fichero, persona):
-        with open(fichero, "r+") as f:
-            lineas = f.readlines()
-            f.seek(0)
+    def elimina_telefono(Self, fichero, persona):
+        with open(fichero, "r+") as archivo:
+            lineas = archivo.readlines()
+            archivo.seek(0)
             for linea in lineas:
                 if persona not in linea:
-                    f.write(linea)
-            f.truncate()
-        pass
+                    archivo.write(linea)
+            archivo.truncate()
 
--- Main.py
+++ Main.py
@@ -2,8 +2,31 @@ import Agenda_Clase
 import Funciones_Utilitarias
 
 
-fichero = "ruta/al/archivo.txt"
+fichero = "agenda.txt"
 agenda = Agenda_Clase.Agenda()
+
+
+def obten_telefono():
+    
+    while True:
+        persona = input ("Nombre de la persona: ")
+        if Funciones_Utilitarias.is_valid_name(persona):
+            agenda.obten_telefono(fichero, persona)
+            exit()
+        
+
+def inserta_telefono():
+    pass
+
+def elimina_telefono():
+    pass
+
+def salir_fun():
+    print("*********************************************")  
+    print("Has salido del programa.")
+    print("*********************************************") 
+    exit()
+
 
 def default():
     print("Selecciona una opción valida dentro del menú")
@@ -15,7 +38,7 @@ cases = {
     "4": salir_fun
 }
 
-def Menu():
+def menu():
     """Función definida para imprimir menú en pantalla y pedir una opción al usuario"""
     salir =True
     while salir:
@@ -32,22 +55,4 @@ def Menu():
         op = input("Seleccione una opción: ").strip()
         cases.get(op, default)()
 
-
-def obten_telefono():
-    Funciones_Utilitarias.is_valid_name()
-    persona = input ("Nombre de la persona: ")
-    agenda.obten_telefono(fichero, persona)
-
-def inserta_telefono():
-    pass
-
-def elimina_telefono():
-    pass
-
-def salir_fun():
-    print("*********************************************")  
-    print("Has salido del programa.")
-    print("*********************************************") 
-    exit()
-
-
+menu ()

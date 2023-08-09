28194c8bec139c4d57f1ed096e7cdda2410e1e45
cesarlongart101 <cesarlongart101@gmail.com>
2023-08-09 22:36:48 +0200

    Agregados comentarios y word

--- Agenda_Clase.py
+++ Agenda_Clase.py
@@ -1,20 +1,48 @@
 class Agenda:
 
     def obten_telefono(Self, fichero, persona):
+        """
+        El método obten_telefono recibe dos parámetos, el primero es el fichero en el cual se leen los datos.
+        El segundo parámetro es persona que es el nombre ingresado por el usuario.
+        Una vez recibidos ambos parámetros se abre el fichero en modo lectura y se itera en cada línea del mismo.
+        Se ejecuta un condicional que valida si el parámetro persona se encuentra en la línea que está iterando, 
+        si se valida se imprime la línea sino se encuentra se imprime un mensaje que dice que el registro no existe.
+        """
         with open(fichero, "r") as archivo:
             for linea in archivo:
                 if persona in linea:
+                    print()
+                    print("El número solicitado es: ")
                     print(linea)
                     return
+            print()
             print("No existe en la agenda.")
 
     def inserta_telefono(Self, fichero, persona, telefono):
+        """
+        El método innserta_telefono recibe tres parámetros, el primero es el fichero, 
+        el cual se abre en modo anexado para que los nuevos datos no sobrescriban los datos existentes.
+        El segundo parámetro es persona que es el nombre ingresado por el usuario.
+        El tercero es el teléfono que corresponde al número de teléfono ingresado por el usuario.
+        Con los tres parámetros recibidos se ejecuta la función write al archivo (fichero) y se le indica agregar en una misma línea 
+        separados por una coma y un espacio el parámetro persona y telefono.
+        """
         with open(fichero, "a") as archivo:
             archivo.write(persona + ", " + telefono + "\n")
 
-
-
     def elimina_telefono(Self, fichero, persona):
+        """
+        El método elimina_telefono recibe dos parámetros, el primero es fichero, el cual se abre en modo lectura.
+        El segundo es persona que es el nombre ingresado por el usuario que desea eliminar.
+        Se ejecuta la funcion readlines() para leer todo el fichero y se guarda en una variable llamada líneas.
+        Se define la variable encontrado con el valor False. 
+        Se itera con la función enumerate así obtenemos la posición (número de línea) y el valor de la línea (nombre y teléfono).
+        Se ejecuta el condicional verificando si el parámetro persona se encuentra en la línea iterada, si es así encontrado es True
+        y se sale del ciclo for. Sino es econtrado se imprime en pantalla que no existe el dato en el archivo.
+        A continuación, si encontrado es True se elimina la posición en la que se detuvo el ciclo for.
+        Se abre nuevamente el fichero en modo escritura, se sobrescribe con el contenido de la variable lineas y se imprime
+        en pantalla que se ha eliminado con éxito.
+        """
         with open(fichero, "r") as archivo:
             lineas = archivo.readlines()
 
@@ -27,7 +55,7 @@ class Agenda:
         if encontrado:
             del lineas[i]
         
-            with open("agenda.txt", "w") as archivo:
+            with open(fichero, "w") as archivo:
                 archivo.writelines(lineas)
             print("*** NOMBRE Y NÚMERO ELIMINADO CON ÉXITO ***")
         else:
--- Main.py
+++ Main.py
@@ -2,23 +2,34 @@ import Agenda_Clase
 import Funciones_Utilitarias
 
 
-fichero = "agenda.txt"
+fichero = "agenda.txt" 
 agenda = Agenda_Clase.Agenda()
 
 
 def obten_telefono():
+    """
+    Función que se usa para buscar un número telefónico dentro de la variable fichero. 
+    Valida los caracteres ingresados por el usuario a través de la función utilitaria is_valid_name. 
+    Llama al método obten_telefono de la clase Agenda validando si el nombre ingresado por el usuario existe.
+    Una vez validado imprime en pantalla el número de teléfono, sino existe muestra un mensaje al usuario.
+    """
     print("*** BUSCAR TELÉFONO ***")
     while True:
-        persona = input ("Nombre de la persona: ")
+        persona = input ("Nombre de la persona: ").upper().strip()
         if Funciones_Utilitarias.is_valid_name(persona):
             agenda.obten_telefono(fichero, persona)
             break
         
 
 def inserta_telefono():
+    """
+    Función que se usa para guardar en el archivo agenda.txt el nombre y número telefónico de un nuevo contacto ingresado por el usuario.
+    Se le solicita al usuario ingresar nombre y número, se validan con las funciones utilitarias is_valid_name y is_valid_phone respectivamente.
+    Una vez validados se llama al método inserta_telefono de la clase Agenda la cual procede a guardar las variables persona y número en el fichero.
+    """
     print("*** GUARDAR EN AGENDA ***")
     while True:
-        persona = input ("Nombre: ")
+        persona = input ("Nombre: ").upper().strip()
         if Funciones_Utilitarias.is_valid_name(persona):
             numero = input ("Número: ")
             if Funciones_Utilitarias.is_valid_phone(numero):
@@ -28,9 +39,14 @@ def inserta_telefono():
         
 
 def elimina_telefono():
+    """
+    Función que se usa para eliminar un registro introducido por el usuario.
+    Se solicita al usuario ingresar el nombre y se valida con la función utilitaria is_valid_name.
+    Una vez validado se llama al método de la clase Ageda la cual procede a eliminar el registro indicado del fichero.
+    """
     print("*** ELIMINAR EN AGENDA ***")
     while True:
-        persona = input ("Nombre: ")
+        persona = input ("Nombre: ").upper().strip()
         if Funciones_Utilitarias.is_valid_name(persona):
             agenda.elimina_telefono(fichero, persona)
             break
